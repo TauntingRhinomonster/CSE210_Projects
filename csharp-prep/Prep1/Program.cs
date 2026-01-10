138fe8a37bc29b7369f31e901f8e7f9f33fd5be1
TauntingRhinomonster <90732061+TauntingRhinomonster@users.noreply.github.com>
2026-01-10 15:32:07 -0700

    Bond, James Bond. This was part 1 of a  Prove assignment for class.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep 1 World!\n");
+        Console.Write("What is your First Name? ");
+        string fname = Console.ReadLine();
+        Console.Write("What is your Last Name? ");
+        string lname = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.\n");
     }
 }
