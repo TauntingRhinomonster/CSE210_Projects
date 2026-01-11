5a1399fde33c78b6ef95718497ec7679701345a8
TauntingRhinomonster <90732061+TauntingRhinomonster@users.noreply.github.com>
2026-01-10 19:45:52 -0700

    The Grade Calculator is finished. It takes in the input from a user and it will calculate the percentage they got based on the corresponding grade.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello Prep 2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+        char grade = 'F';
+        string sign = "";
+
+        // Checking what the grade is
+        if (percent >= 90)
+        {
+            grade = 'A';
+        }
+        else if (percent >= 80)
+        {
+            grade = 'B';   
+        }
+        else if (percent >= 70)
+        {
+            grade = 'C';
+        }
+        else if (percent >= 60)
+        {
+            grade = 'D';
+        };
+
+        if ((percent % 10 >= 5) || (percent >= 100))
+        {
+            sign = "+";
+        }
+        else
+        {
+            sign = "-";
+        }
+
+        // Fixing the A and F grading issue
+        if (grade == 'A' && sign == "+")
+        {
+            sign = "";
+        }
+        else if (grade == 'F')
+        {
+            sign = "";
+        }
+
+        // Writing out the solution
+        Console.WriteLine($"Your grade is: '{grade}{sign}'");
+       
+        // Telling the user if they passed their class
+        if (percent < 70)
+        {
+            Console.WriteLine("You failed. Maybe try studying more. That might help.");
+        }
+        else
+        {
+            Console.WriteLine("You passed! Hurray!");
+        }
     }
 }
