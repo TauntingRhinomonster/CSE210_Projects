23532bc0a0499d1e21fdcfe29363b20677f7071c
TauntingRhinomonster <90732061+TauntingRhinomonster@users.noreply.github.com>
2026-01-14 12:39:23 -0700

    Finished the number guessing game and did all of the stretch goals.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,31 +1,43 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep 3 World!");
-        Console.Write("What is the magic number? ");
-        int magic = int.Parse(Console.ReadLine());
-        Console.Write("What is your guess? ");
-        int number = int.Parse(Console.ReadLine());
-
-        while (number != magic)
+        Console.WriteLine("Would you like to play a number guessing game? (y/n)");
+        string answer = Console.ReadLine();
+        while (answer == "y")
         {
-            Console.Write("Not quite... ");
-            if (number < magic)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if (number > magic)
-            {
-                Console.WriteLine("Lower");
-            }
+            Random random = new Random();
+            int magic = random.Next(101);
+            int guesses = 0;
+            Console.WriteLine("There is a random number you need to guess from 0-100.");
             Console.Write("What is your guess? ");
-            number = int.Parse(Console.ReadLine());
+            int number = int.Parse(Console.ReadLine());
+            guesses += 1;
 
+            while (number != magic)
+            {
+                Console.Write("Not quite... ");
+                if (number < magic)
+                {
+                    Console.WriteLine("Higher");
+                    guesses += 1;
+                }
+                else if (number > magic)
+                {
+                    Console.WriteLine("Lower");
+                    guesses += 1;
+                }
+                Console.Write("What is your guess? ");
+                number = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine($"You Guessed It!\nIt took you {guesses} tries.");
+            Console.WriteLine("Would you like to play again? (y/n)");
+            answer = Console.ReadLine();
         }
-        Console.WriteLine("You Guessed It!");
-
+        Console.WriteLine("Bye bye!");
     }
 }
