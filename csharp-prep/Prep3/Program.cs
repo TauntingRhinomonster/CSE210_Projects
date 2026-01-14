ffddcd83efb8408c45c8f15342366f32279734c4
TauntingRhinomonster <90732061+TauntingRhinomonster@users.noreply.github.com>
2026-01-14 12:12:30 -0700

    Number guessing game works with user inputting what the magic number is at the start. I will now begin adding the random number generation.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Hello Prep 3 World!");
+        Console.Write("What is the magic number? ");
+        int magic = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int number = int.Parse(Console.ReadLine());
+
+        while (number != magic)
+        {
+            Console.Write("Not quite... ");
+            if (number < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess? ");
+            number = int.Parse(Console.ReadLine());
+
+        }
+        Console.WriteLine("You Guessed It!");
+
     }
 }
